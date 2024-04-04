6cd56a3df68473f874a23b66d9e793553fd80a85
Kamil Kucharski <kamil.kucharski@makimo.pl>
2024-04-04 21:03:26 +0200

    Run simple TCP server

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+const PORT: &str = "8888";
+const HOST: &str = "127.0.0.1";
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let reader = BufReader::new(&mut stream);
+
+    let data: Vec<String> = reader
+        .lines()
+        .filter_map(|result| result.ok())
+        .collect();
+
+    println!("Received data from client {}: {:#?}", stream.peer_addr().unwrap(), data.join(","));
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let socket = TcpListener::bind(format!("{HOST}:{PORT}"))?;
+
+    for stream in socket.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
